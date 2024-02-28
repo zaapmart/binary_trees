b973041c9e4b965bd70f8e1d699f5deb124c1203
Kufre Akpan <contactkufreakpan@gmail.com>
2024-02-28 13:22:06 -0800

    fixed betty issues

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,43 +10,44 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    /* Check if parent is NULL */
-    if (parent == NULL)
-        return (NULL);
+	/* Check if parent is NULL */
+	if (parent == NULL)
+		return (NULL);
 
-    /* Allocate memory for the new node */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /* Assign the value to the new node */
-    new_node->n = value;
+	/* Assign the value to the new node */
+	new_node->n = value;
 
-    /* Set the parent of the new node */
-    new_node->parent = parent;
+	/* Set the parent of the new node */
+	new_node->parent = parent;
 
-    /* If parent already has a left child */
-    if (parent->left != NULL)
-    {
-        /* Set the left child of the new node to the current left child */
-        new_node->left = parent->left;
-        /* Set the parent of the current left child to the new node */
-        parent->left->parent = new_node;
-    }
-    else
-    {
-        /* If parent doesn't have a left child, set new node's left child to NULL */
-        new_node->left = NULL;
-    }
+	/* If parent already has a left child */
+	if (parent->left != NULL)
+	{
+		/* Set the left child of the new node to the current left child */
+		new_node->left = parent->left;
+		/* Set the parent of the current left child to the new node */
+		parent->left->parent = new_node;
+	}
+	else
+	{
+		/* If parent doesn't have a left child, set new node's left child to NULL */
+		new_node->left = NULL;
+	}
 
-    /* Set the left child of the parent to the new node */
-    parent->left = new_node;
+	/* Set the left child of the parent to the new node */
+	parent->left = new_node;
 
-    /* Set the right child of the new node to NULL */
-    new_node->right = NULL;
+	/* Set the right child of the new node to NULL */
+	new_node->right = NULL;
 
-    /* Return a pointer to the new node */
-    return (new_node);
+	/* Return a pointer to the new node */
+	return (new_node);
 }
+
